dc1cd7df4854d3ea9c96c35862174b7d2f283531
Steven-Vasquez <70172054+Steven-Vasquez@users.noreply.github.com>
2022-11-28 11:36:03 -0800

    Update State.tsx

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -9,6 +9,9 @@ import { WaveformVisualizer } from './visualizers/Waveform';
 import { pitchVisualizer } from './visualizers/ajccarlson';
 import { SpectrumVisualizer } from './visualizers/Spectrum';
 
+import { StevensDrums } from './instruments/Steven-Vasquez-1';
+import { StevenVisualizer } from './visualizers/Steven-Vasquez-1';
+
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -19,13 +22,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, TripleOSCInstrument, slideWhistleInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, TripleOSCInstrument, slideWhistleInstrument, StevensDrums]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer, SpectrumVisualizer, pitchVisualizer, faiyazc]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, SpectrumVisualizer, pitchVisualizer, faiyazc, StevenVisualizer]);    // similar to Visualizer[]
 
 
 /**
